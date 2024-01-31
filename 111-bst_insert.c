c68def8107f9274a664cd7da5090aa01f46a975f
Khanyicou <preciouskhanyisa1@gmail.com>
2024-01-31 11:02:08 -0800

    ou

---
+++ 111-bst_insert.c
@@ -0,0 +1,44 @@
+#include "binary_trees.h"
+
+/**
+ * bst_insert -  insert nodes in order to create a BST tree
+ * @tree: tree to create with type BST
+ * @value: value of node to insert
+ * Return:  BST tree
+ */
+bst_t *bst_insert(bst_t **tree, int value)
+{
+	bst_t *current, *new;
+
+	if (tree != NULL)
+	{
+		current = *tree;
+		if (current == NULL)
+		{
+			new = binary_tree_node(current, value);
+			if (new == NULL)
+				return (NULL);
+			return (*tree = new);
+		}
+		if (value < current->n)
+		{
+			if (current->left != NULL)
+				return (bst_insert(&current->left, value));
+			new = binary_tree_node(current, value);
+			if (new == NULL)
+				return (NULL);
+			return (current->left = new);
+		}
+		if (value > current->n)
+		{
+			if (current->right != NULL)
+				return (bst_insert(&current->right, value));
+			new = binary_tree_node(current, value);
+			if (new == NULL)
+				return (NULL);
+			return (current->right = new);
+		}
+	}
+
+	return (NULL);
+}
